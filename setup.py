990c47c9396ccd05fe7c21a34ee7a6c33445ab0c
wouter bolsterlee <wouter@eclecticiq.com>
2017-10-05 20:22:35 +0200

    add long_description metadata field

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
+import os
 from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fp:
+    long_description = fp.read()
 
 setup(
     name='datauri',
     description="implementation of the data uri scheme defined in rfc2397",
+    long_description=long_description,
     version='1.0.0',
     author="EclecticIQ",
     author_email="engineering@eclecticiq.com",
