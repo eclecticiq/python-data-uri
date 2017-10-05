deb3a6b49e3426c7ac81c334df754034770f2d1a
wouter bolsterlee <wouter@eclecticiq.com>
2017-10-05 19:28:45 +0200

    add setup script

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='datauri',
+    description="implementation of the data uri scheme defined in rfc2397",
+    version='1.0.0',
+    author="EclecticIQ",
+    author_email="engineering@eclecticiq.com",
+    packages=['datauri'],
+    url='https://github.com/eclecticiq/python-data-uri',
+    license="BSD",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+)
